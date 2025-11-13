4371e85b5affe1a03c06d4fba7d57d92ff950f40
Rebin <rteimouri13@gmail.com>
2025-11-13 14:17:11 +0100

    saving incase of problems

--- Examination 1/Task 3/task3.c
+++ Examination 1/Task 3/task3.c
@@ -3,5 +3,28 @@
 #include <stdio.h>
 
 int main(void){
+    float price = 0;
+    float tax = 0;
+    float total = 0;
+    float vat = 0;
+
+    printf("Price of article (including VAT): ");
+    scanf("%f", &price);
+
+    printf("Enter VAT: %%");
+    scanf("%f", &tax);
+
+    tax / 100;
+    tax + 1;
     
+
+    printf("%f", tax);
+
+
+    printf("The price of the product is %f\n", price);
+    printf("The VAT of the product is: %% %f\n", tax);
+    printf("Price excluding VAT is: %f", total);
+    printf("The VAT is: ", vat);
+
+    return 0;
 }
